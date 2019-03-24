dc026d08e4ab5725fba96aca1a2db5aa3252d2c0
754623476 <47409440+754623476@users.noreply.github.com>
2019-03-24 10:22:06 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+var user = "Person15";
+    var salutation = "saluations";
+        var greeting = user + "says" + saluation + "!"
+var product price = 500
+    var student discount percentage = .1
+        var student price = product price - (product price * student discount percentage)
+        
+var cars = ["Setting Sync","Shopify Liquid Snippets","HTML Snippets"]
+
+function Package(SettingSync, ShopifyLiquidSnippets, HTMLSnippets) {
+  this.SettingSync = Setting Sync;
+  this.ShopifyLiquidSnippets = Shopify Liquid Snippets;
+  this.HTMLSnippets  = HTML Snippets;
+}
+        
+        
+        
